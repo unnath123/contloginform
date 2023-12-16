4597ea02929169a9a0deaa79b74933affdc23166
unnath123 <unnathpgowda@gmail.com>
2023-12-16 17:54:32 +0530

    complete

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -5,7 +5,8 @@ const LoginForm = () =>{
     const [user,setUser] = useState({email:"", password:"", cfpassword:""})
     const {email, password, cfpassword} = user
     const [pmatch, setPmatch] = useState(false)
-    const [plen, setPlen] = useState(false)
+    const [plen, setPlen] = useState(false);
+    const [fillp, setFillp] = useState(false)
 
     function handleInput(e){
         setUser({...user, [e.target.name]:e.target.value})
@@ -20,17 +21,19 @@ const LoginForm = () =>{
         e.preventDefault();
         if(!email || !password){
             setErrEmail(true)
-            setPmatch(true)
+            // setPmatch(true)
+            setFillp(true)
         }
         else {
             if(validateEmail()){
+                setFillp(false)
                 setErrEmail(false)
                 console.log("email validated")
 
-                if(password.length> 8 ){
+                if(password.length> 3 ){
                     setPlen(false)
                     if(password === cfpassword){
-                        setErrEmail(false)
+                        // setErrEmail(false)
                         setPmatch(false)
                         alert("user signup successful")
                     }
@@ -48,12 +51,18 @@ const LoginForm = () =>{
         }
     }
     return(
-        <div>
+        <div className='container'>
             <form onSubmit={handleSubmit}>
-                <input type='text'value={user.email} name='email' onChange={handleInput} placeholder='Email' />
+                <label>Email:</label>
+                <input type='text'value={user.email} name='email' onChange={handleInput} placeholder='Email'  />
                     {erremail && <p>email format not valid</p> }
+                
+                <label>Password</label>
                 <input type='password' name='password' value={user.password} onChange={handleInput} placeholder='Password'  />
                     {plen && <p>password length should be greater than 8</p>}
+                    {fillp && <p>Password cannot be empty</p>}
+
+                <label>Confirm password</label>
                 <input type='password' value={user.cfpassword} name='cfpassword' onChange={handleInput} placeholder='confirm-password' />
                     { pmatch && <p>Passwords doesnt match</p> } 
                 <button type='submit'>Login</button>
